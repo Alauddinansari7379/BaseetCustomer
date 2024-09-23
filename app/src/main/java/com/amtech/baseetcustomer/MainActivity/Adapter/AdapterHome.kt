d674eefd3b7defed096667829319173366f4d23a
Alauddin <alauddinansari7379@gmail.com>
2024-09-23 14:59:35 +0530

    Remaining Amt Done

---
+++ app/src/main/java/com/amtech/baseetcustomer/MainActivity/Adapter/AdapterHome.kt
@@ -0,0 +1,157 @@
+package com.amtech.baseetcustomer.MainActivity.Adapter
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.amtech.baseetcustomer.AddService.BookingDetail
+import com.amtech.baseetcustomer.MainActivity.Model.ModelGetTranslatorItem
+import com.amtech.baseetcustomer.R
+import com.amtech.baseetcustomer.databinding.SingleRowCarBinding
+import com.amtech.baseetcustomer.sharedpreferences.SessionManager
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+
+
+class AdapterHome(
+    val context: Context,
+    var list: ArrayList<ModelGetTranslatorItem>,
+) : RecyclerView.Adapter<AdapterHome.ViewHolder>() {
+    private lateinit var sessionManager: SessionManager
+
+    inner class ViewHolder(val binding: SingleRowCarBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            SingleRowCarBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    @SuppressLint("SetTextI18n", "ResourceAsColor")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        sessionManager = SessionManager(context)
+        try {
+            with(holder) {
+                with(list[position]) {
+                    binding.tvName.text =  context.resources.getString(R.string.name) +name
+                    binding.tvTime.text = context.resources.getString(R.string.Time)  +start_time +context.resources.getString(R.string.to) +end_time
+                    binding.tvCountry.text = country
+                    binding.tvDates.text = serv_date
+                    binding.tvHomeType.text = type
+                    binding.tvPriceN.text = context.resources.getString(R.string.USD) +price
+                    val jsonArray = JsonArray()
+                    jsonArray.add("0")
+                    val jsonObject = JsonObject()
+                    jsonObject.add("accept_by", jsonArray)
+
+                    val jsonString = "$accept_by"
+
+                    println(jsonString)
+                    Log.e("accept_by", jsonString)
+
+                    if (jsonString.contains("0")) {
+                        binding.tvRequested.text = context.resources.getString(R.string.Requested)
+                        binding.layoutRequested.setBackgroundTintList(
+                            ColorStateList.valueOf(
+                                ContextCompat.getColor(
+                                    context,
+                                    R.color.main_color
+                                )
+                            )
+                        )
+                    }
+
+//                    else if (jsonString.contains("5") && status.toString() == "2") {
+//                        binding.tvRequested.text = "Completed"
+//                        binding.layoutRequested.setBackgroundTintList(
+//                            ColorStateList.valueOf(
+//                                ContextCompat.getColor(
+//                                    context,
+//                                    com.amtech.baseetcustomer.R.color.green
+//                                )
+//                            )
+//                        )
+//                    }
+
+                    else {
+                        binding.tvRequested.text = context.resources.getString(R.string.pending)
+                        binding.layoutRequested.setBackgroundTintList(
+                            ColorStateList.valueOf(
+                                ContextCompat.getColor(
+                                    context,
+                                    com.amtech.baseetcustomer.R.color.blue
+                                )
+                            )
+                        )
+                    }
+
+
+                    binding.layoutRequested.setOnClickListener {
+                        if (serv_id.isNotEmpty() && binding.tvRequested.text != "Completed") {
+                            var foodId = ""
+                            var aminetes = ""
+                            var homeType = ""
+                            var serviceDate = "NA"
+                            for (i in serv_id) {
+                                foodId = i.id.toString()
+                                aminetes = i.amenities
+                                homeType = i.car_type
+                            }
+                            if (!serv_date.isNullOrEmpty()) {
+                                serviceDate = serv_date.toString()
+                            }
+                            val i = Intent(context, BookingDetail::class.java)
+                                .putExtra("callFrom", "Home")
+                                .putExtra("statues", binding.tvRequested.text.toString())
+                                .putExtra("id", id.toString())
+                                .putExtra("foodId", foodId)
+                                .putExtra("name", name)
+                                .putExtra("tr_from", tr_from)
+                                .putExtra("tr_to", tr_to)
+                                .putExtra("start_time", start_time)
+                                .putExtra("end_time", end_time)
+                                .putExtra("description", description)
+                                .putExtra("country", country)
+                                .putExtra("type", type)
+                                .putExtra("trperson", trperson)
+                                .putExtra("driv_type", driv_type)
+                                .putExtra("serv_date", serviceDate)
+                                .putExtra("aminetes", aminetes)
+                                .putExtra("price", price!!)
+                                .putExtra("homeType", homeType)
+                            context.startActivity(i)
+                        }
+
+                    }
+
+//
+//                if (list[position].serv_id.appimage != null) {
+//                    Picasso.get().load("https:"+list[position].serv_id.appimage)
+//                        .placeholder(R.drawable.usernew)
+//                        .error(R.drawable.usernew)
+//                        .into(binding.image)
+//
+//                }
+
+
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+//    interface Cart{
+//        fun addToCart(toString: String)
+//    }
+}
---
+++ app/src/main/java/com/amtech/baseetcustomer/MainActivity/Adapter/AdapterTranslator.kt
@@ -0,0 +1,142 @@
+package com.amtech.baseetcustomer.MainActivity.Adapter
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.amtech.baseetcustomer.AddService.BookingDetail
+import com.amtech.baseetcustomer.AddService.OrderDetail
+import com.amtech.baseetcustomer.Helper.pmFormate
+import com.amtech.baseetcustomer.MainActivity.Model.ModelGetTranslatorItem
+import com.amtech.baseetcustomer.R
+import com.amtech.baseetcustomer.databinding.SingleRowTranslatorBinding
+import com.amtech.baseetcustomer.sharedpreferences.SessionManager
+import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+
+
+class AdapterTranslator(
+    val context: Context,
+    var list: ArrayList<ModelGetTranslatorItem>,/*private val videoCall: VideoCall*/
+ ) : RecyclerView.Adapter<AdapterTranslator.ViewHolder>() {
+    private lateinit var sessionManager: SessionManager
+
+    inner class ViewHolder(val binding: SingleRowTranslatorBinding) : RecyclerView.ViewHolder(binding.root)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = SingleRowTranslatorBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+    }
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    @SuppressLint("SetTextI18n", "ResourceAsColor")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        sessionManager= SessionManager(context)
+        try {
+            with(holder) {
+                with(list[position]) {
+                     binding.tvName.text = context.resources.getString(R.string.name) +name
+                     binding.tvLangauge.text = context.resources.getString(R.string.Language)+ tr_from +context.resources.getString(R.string.to)+tr_to
+                      binding.tvTime.text = context.resources.getString(R.string.Time)+ start_time+context.resources.getString(R.string.to)+end_time
+                    binding.tvType.text = type
+                    binding.tvDates.text = serv_date
+                    binding.tvPrice.text = price
+                    binding.tvPriceN.text = "USD $price"
+                     val jsonArray = JsonArray()
+                    jsonArray.add("0")
+                    val jsonObject = JsonObject()
+                    jsonObject.add("accept_by", jsonArray)
+
+                     val jsonString = "$accept_by"
+
+                    println(jsonString)
+                    Log.e("accept_by",jsonString)
+
+                    if (jsonString.contains("0")){
+                        binding.tvRequested.text =context.resources.getString(R.string.Requested)
+                         binding.layoutRequested.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(context, com.amtech.baseetcustomer.R.color.main_color)))
+                     }
+//                    else if (jsonString.contains("5")&& status.toString()=="2"){
+//                        binding.tvRequested.text ="Completed"
+//                        binding.layoutRequested.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(context, com.amtech.baseetcustomer.R.color.green)))
+//                    }
+                    else{
+                        binding.tvRequested.text =context.resources.getString(R.string.pending)
+                         binding.layoutRequested.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(context, com.amtech.baseetcustomer.R.color.blue)))
+                    }
+
+
+
+                    binding.layoutRequested.setOnClickListener {
+                        if (serv_id.isNotEmpty() && binding.tvRequested.text!="Completed") {
+                            var foodId = ""
+                            var serviceDate = ""
+                            for (i in serv_id) {
+                                foodId = i.id.toString()
+                            }
+                            if (!serv_date.isNullOrEmpty()){
+                                serviceDate=serv_date.toString()
+                            }
+                            val i = Intent(context, BookingDetail::class.java)
+                                .putExtra("callFrom", "Translator")
+                                .putExtra("statues", binding.tvRequested.text.toString())
+                                .putExtra("id", id.toString())
+                                .putExtra("foodId", foodId)
+                                .putExtra("name", name)
+                                .putExtra("tr_from", tr_from)
+                                .putExtra("tr_to", tr_to)
+                                .putExtra("start_time", start_time)
+                                .putExtra("end_time", end_time)
+                                .putExtra("description", description)
+                                .putExtra("country", country)
+                                .putExtra("type", type)
+                                .putExtra("serv_date", serviceDate)
+                                .putExtra("price", price!!)
+                            context.startActivity(i)
+                        }
+                    }
+
+//                    if (type=="On Call" && !jsonString.contains("0")){
+//                        binding.layoutVideoCall.visibility= View.VISIBLE
+//                    }else{
+//                        binding.layoutVideoCall.visibility= View.GONE
+//                    }
+//
+//
+//                    binding.imgVideoCall.setOnClickListener {
+//                        videoCall.videoCall("Service$customer_id")
+//                    }
+//
+//                if (list[position].serv_id.appimage != null) {
+//                    Picasso.get().load("https:"+list[position].serv_id.appimage)
+//                        .placeholder(R.drawable.usernew)
+//                        .error(R.drawable.usernew)
+//                        .into(binding.image)
+//
+//                }
+
+
+                }
+            }
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+    }
+
+//    interface VideoCall{
+//        fun videoCall(toString: String)
+//    }
+
+//    interface Cart{
+//        fun addToCart(toString: String)
+//    }
+}
